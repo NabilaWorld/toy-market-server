f480b84e33357074ac200ad44e5b8f68cddca732
NabilaWorld <mnabila30163@gmail.com>
2023-05-19 00:29:33 +0600

    commit the index.js file setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleWare
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('girls cooking toy')
+})
+
+app.listen(port, ()=>{
+    console.log(`Girls are happy ${port}`)
+})
