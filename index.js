51c7504bfd162b42cbd7ca1142d62b1c69e9bb7d
NabilaWorld <mnabila30163@gmail.com>
2023-05-21 08:00:22 +0600

    commit the next update

--- index.js
+++ index.js
@@ -132,7 +132,13 @@ async function run() {
       res.send(result);
     });
 
-
+// delete data
+app.delete('/myToy/:id', async(req, res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await addToy.deleteOne(query);
+  res.send(result);
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
